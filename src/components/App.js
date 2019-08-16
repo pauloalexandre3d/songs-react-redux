ed8478c2c90d96b543a5f117521e895b4d731e8e
Paulo Machado <pauloalexandre3d@gmail.com>
2019-08-16 23:44:07 +0100

    feature: Adding SongList component and connecting it Redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { selectSong } from '../actions';
+import SongList from './SongList';
 
 class App extends React.Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container grid">
+        <div className="ui row">
+          <div className="column eight wide">
+            <SongList />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
